76622582c17be5bed146f99b2ca3d6d11a1909a2
User <qianxun114514@gmail.com>
2025-11-18 19:11:53 +0800

    修复编译

--- Source/MinecraftEditor.Target.cs
+++ Source/MinecraftEditor.Target.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public class MinecraftEditorTarget : TargetRules
 {
-	public MinecraftEditorTarget(TargetInfo Target) : base(Target)
+	public MinecraftEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
